c44f1c91b5a64e4074c2708dc0ef56cd4ba953fd
nin <ninian138@gmail.com>
2017-07-25 17:30:26 +0900

    refactor module Search

--- app/src/main/java/jp/gr/java_conf/item/recizo/module/Search.kt
+++ app/src/main/java/jp/gr/java_conf/item/recizo/module/Search.kt
@@ -1,6 +1,5 @@
 package jp.gr.java_conf.item.recizo.module
 
-import android.support.v7.app.AppCompatActivity
 import jp.gr.java_conf.item.recizo.contract.CookpadCallBack
 import jp.gr.java_conf.item.recizo.contract.ProgressBarCallBack
 import jp.gr.java_conf.item.recizo.model.ErrorCode
@@ -14,55 +13,49 @@ import java.io.IOException
 import java.lang.Exception
 import kotlin.properties.Delegates
 
-abstract class Search: AppCompatActivity() {
-    var urlBase = ""
-    var queryString = ""
-    var numberOfItem: Int by Delegates.notNull()
-    var totalNumberOfPages = 0
-    var nowPage = 0
+abstract class Search {
+  var urlBase = ""
+  var queryString = ""
+  var numberOfItem: Int by Delegates.notNull()
+  var totalNumberOfPages = 0
+  var nowPage = 0
 
-    fun scrapingHTML(progressCallback: ProgressBarCallBack, callback: CookpadCallBack) = launch(UI) {
-        progressCallback.progressBarStart()
-        val html = getHTML(getSearchUrl(), nowPage).await()
-        progressCallback.progressBarStop()
-
-        when(html) {
-            "IoError" -> callback.failed(ErrorCode.IO_ERROR)
-            "OtherError" -> callback.failed(ErrorCode.GENERIC_ERROR)
-            else -> {
-                numberOfItem = parseNumberOfItem(html as Document)
-                totalNumberOfPages = Math.ceil((numberOfItem.toDouble() / 10)).toInt()
-                callback.succeed(html)
-            }
-        }
+  fun scrapingHTML(progressCallback: ProgressBarCallBack, callback: CookpadCallBack) = launch(UI) {
+    progressCallback.progressBarStart()
+    val html = getHTML(getSearchUrl(), nowPage).await()
+    progressCallback.progressBarStop()
+    when(html) {
+      "IoError" -> callback.failed(ErrorCode.IO_ERROR)
+      "OtherError" -> callback.failed(ErrorCode.GENERIC_ERROR)
+      else -> {
+        numberOfItem = parseNumberOfItem(html as Document)
+        totalNumberOfPages = Math.ceil((numberOfItem.toDouble() / 10)).toInt()
+        callback.succeed(html)
+      }
     }
+  }
 
-    fun pageToNext():Boolean {
-        if(nowPage > totalNumberOfPages) {
-            return false
-        } else {
-            nowPage++
-            return true
-        }
+  fun pageToNext():Boolean {
+    if(nowPage > totalNumberOfPages) return false
+    else {
+      nowPage++
+      return true
     }
-
-    fun pageToPrev(): Boolean {
-        if(nowPage < 1) {
-            return false
-        } else {
-            nowPage--
-            return true
-        }
+  }
+  fun pageToPrev(): Boolean {
+    if(nowPage < 1) return false
+    else {
+      nowPage--
+      return true
     }
+  }
 
-    private fun getHTML(url: String, pageNum: Int = 1) = async(CommonPool) {
-        // TODO ProgressSpinnerの追加
-        val result = try {Jsoup.connect(url + queryString + pageNum).get()} catch (e: IOException) {"IoError"} catch (e:Exception){"OtherError"}
+  private fun getHTML(url: String, pageNum: Int = 1) = async(CommonPool) {
+    // TODO ProgressSpinnerの追加
+    val result = try {Jsoup.connect(url + queryString + pageNum).get()} catch (e: IOException) {"IoError"} catch (e:Exception){"OtherError"}
+    return@async result
+  }
 
-        return@async result
-    }
-
-    abstract protected fun parseNumberOfItem(html: Document): Int
-
-    abstract protected fun getSearchUrl(): String
+  abstract protected fun parseNumberOfItem(html: Document): Int
+  abstract protected fun getSearchUrl(): String
 }
