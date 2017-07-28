6debeffefac2b48375dd9935717ed8cfd0922cc7
minakawa <minakawa.daiki.as@gmail.com>
2017-07-28 12:34:16 +0900

    Add link jump

--- app/src/main/java/com/recizo/presenter/FlyerListAdapter.kt
+++ app/src/main/java/com/recizo/presenter/FlyerListAdapter.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 import com.recizo.R
 import com.recizo.model.entity.ShufooFlyer
 
-class FlyerListAdapter: RecyclerView.Adapter<FlyerListAdapter.FlyerViewHolder>() {
+class FlyerListAdapter(private val recyclerView: RecyclerView): RecyclerView.Adapter<FlyerListAdapter.FlyerViewHolder>() , View.OnClickListener {
 
   inner class FlyerViewHolder(v: View): RecyclerView.ViewHolder(v){
     val storeName: TextView = v.findViewById(R.id.shufoo_shopName)
@@ -16,26 +16,42 @@ class FlyerListAdapter: RecyclerView.Adapter<FlyerListAdapter.FlyerViewHolder>()
     val description: TextView = v.findViewById(R.id.shufoo_description)
     //val imageUrl: ImageView = v.findViewById(R.id.recipe_image)
   }
-  val FlyerList = mutableListOf<ShufooFlyer>()
+  val flyerList = mutableListOf<ShufooFlyer>()
+  private var onItemClickListener: FlyerListAdapter.OnItemClickListener? = null
+
+  fun setOnItemClickListener(listener: FlyerListAdapter.OnItemClickListener){
+    onItemClickListener = listener
+  }
 
   override fun getItemCount(): Int {
-    return FlyerList.size
+    return flyerList.size
   }
 
   override fun onBindViewHolder(holder: FlyerViewHolder?, position: Int) {
-    holder!!.storeName.text = FlyerList[position].storeName
-    holder.shufooLink.text = FlyerList[position].shufooLink
-    holder.description.text = FlyerList[position].description
+    holder!!.storeName.text = flyerList[position].storeName
+    holder.shufooLink.text = flyerList[position].shufooLink
+    holder.description.text = flyerList[position].description
   }
 
   override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FlyerViewHolder {
     val v = LayoutInflater.from(parent!!.context).inflate(R.layout.flyer_list_item, parent, false)
+    v.setOnClickListener(this)
     return FlyerViewHolder(v)
   }
 
   fun addFlyer(flyer: ShufooFlyer) {
-    FlyerList.add(flyer)
-    notifyItemInserted(FlyerList.size)
+    flyerList.add(flyer)
+    notifyItemInserted(flyerList.size)
+  }
+
+  override fun onClick(view: View?) {
+    val position = recyclerView.getChildAdapterPosition(view)
+    onItemClickListener?.onItemClick(this.flyerList[position])
+  }
+
+
+  interface OnItemClickListener {
+    fun onItemClick(flyer: ShufooFlyer)
   }
 
 }
