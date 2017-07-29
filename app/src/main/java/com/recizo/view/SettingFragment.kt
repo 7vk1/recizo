9acc6409bd5c351b493032976db7ddbb4f6cbfad
minakawa <minakawa.daiki.as@gmail.com>
2017-07-29 17:27:35 +0900

    Add AboutMeActivity and LicenceActivity

--- app/src/main/java/com/recizo/view/SettingFragment.kt
+++ app/src/main/java/com/recizo/view/SettingFragment.kt
@@ -1,21 +1,21 @@
 package com.recizo.view
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceFragment
 import com.recizo.R
 import android.preference.Preference
 import android.preference.PreferenceManager
 import com.recizo.module.AppContextHolder
-
-
-
+import com.recizo.setting_avtivitys.AboutMeActivity
+import com.recizo.setting_avtivitys.AlertSettingActivity
+import com.recizo.setting_avtivitys.LicenceActivity
 
 class SettingFragment : PreferenceFragment() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     addPreferencesFromResource(R.xml.preferences)
-
 
     // postcode setting
     val editTextPreference = findPreference("edit_postcode_key")
@@ -28,18 +28,32 @@ class SettingFragment : PreferenceFragment() {
       }else false
     }
 
-
     // alert settings
     val alertPreference = findPreference("alert_item") as SettingItemView
     alertPreference.summary = "1日前8時"
     alertPreference.setListOnClickListener(object : SettingItemView.OnListClickListener{
       override fun onListClick() {
-
+        activity.startActivity(Intent(activity, AlertSettingActivity::class.java))
       }
     })
 
+    // about this app
+    (findPreference("about_me_item") as SettingItemView).setListOnClickListener(
+            object : SettingItemView.OnListClickListener{
+              override fun onListClick() {
+                activity.startActivity(Intent(activity, AboutMeActivity::class.java))
+              }
+            }
+    )
 
-    //  about this app
 
+    // ライセンス
+    (findPreference("licence_item") as SettingItemView).setListOnClickListener(
+            object : SettingItemView.OnListClickListener{
+              override fun onListClick() {
+                activity.startActivity(Intent(activity, LicenceActivity::class.java))
+              }
+            }
+    )
   }
 }
