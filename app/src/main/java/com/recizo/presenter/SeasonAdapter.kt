1249dcd1b6d8d0c413b0ff4d9a7d59e426d63198
sugarnaoming <sakigithub@gmail.com>
2017-07-30 15:46:20 +0900

    Polish SeasonAdapter

--- app/src/main/java/com/recizo/presenter/SeasonAdapter.kt
+++ app/src/main/java/com/recizo/presenter/SeasonAdapter.kt
@@ -10,52 +10,50 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.github.mikephil.charting.data.LineRadarDataSet
 import com.recizo.R
 import kotlin.properties.Delegates
 
 class SeasonAdapter(val context: Context,val seasonList: List<String>): RecyclerView.Adapter<SeasonAdapter.SeasonViewHolder>() {
-  init {
-    this.setHasStableIds(true)
+  init {this.setHasStableIds(true)}
+  private fun isDivider(name: String): Boolean {
+    when(name) {
+      "野菜" -> return true
+      "魚・貝" -> return true
+      "芋・キノコ" -> return true
+      "果物・きのみ" -> return true
+      else -> return false
+    }
   }
-
-  override fun getItemCount(): Int {
-    return seasonList.size
-  }
-
-  override fun onBindViewHolder(holder: SeasonViewHolder?, position: Int) {
-    val pos = holder!!.itemId.toInt()
-    holder.name.text = seasonList[pos]
-    PropertyHolder.nameSize = holder.name.textSize
-    PropertyHolder.framePaddginTop = holder.frame.paddingTop
-    PropertyHolder.framePaddginBottom = holder.frame.paddingBottom
-    PropertyHolder.framePaddginStart = holder.frame.paddingStart
-    PropertyHolder.framePaddginEnd = holder.frame.paddingEnd
-
-    if(isDivider(seasonList[pos])) dividerSetting(holder.frame, holder.name)
-    else columnSetting(holder.frame, holder.name)
+  
+  private fun setFramePadding(frame: LinearLayout, start_multiple: Int, top_plus: Int, end_multiple: Int, bottom_plus: Int) {
+    val s = PropertyHolder.framePaddingStart!!.times(start_multiple)
+    val e = PropertyHolder.framePaddingEnd!!.times(end_multiple)
+    val t = PropertyHolder.framePaddingTop!!.plus(top_plus)
+    val b = PropertyHolder.framePaddingBottom!!.plus(bottom_plus)
+    frame.setPaddingRelative(s, e, t, b)
   }
 
   private fun dividerSetting(frame: LinearLayout, name: TextView) {
     frame.setBackgroundColor(context.resources.getColor(R.color.colorAccent))
-    val start = PropertyHolder.framePaddginStart!!.times(1)
-    val end = PropertyHolder.framePaddginEnd!!.times(1)
-    val top = PropertyHolder.framePaddginTop!!.plus(0)
-    val bottom = PropertyHolder.framePaddginBottom!!.plus(0)
-    frame.setPaddingRelative(start, top, end, bottom)
-    name.gravity = Gravity.DISPLAY_CLIP_VERTICAL
+    setFramePadding(frame, 1, 0, 1, 0)
     name.textSize = PropertyHolder.nameSize!!.times(0.5f)
   }
 
   private fun columnSetting(frame: LinearLayout, name: TextView) {
     frame.setBackgroundColor(Color.WHITE)
-    val start = PropertyHolder.framePaddginStart!!.times(2)
-    val end = PropertyHolder.framePaddginEnd!!.times(2)
-    val top = PropertyHolder.framePaddginTop!!.plus(10)
-    val bottom = PropertyHolder.framePaddginBottom!!.plus(0)
-    frame.setPaddingRelative(start, top, end, bottom)
+    setFramePadding(frame, 2, 10, 2, 0)
     name.textSize = PropertyHolder.nameSize!!.times(0.25f)
   }
-
+  
+  private fun setPropertyHolder(holder: SeasonViewHolder?) {
+    PropertyHolder.nameSize = holder!!.name.textSize
+    PropertyHolder.framePaddingTop = holder.frame.paddingTop
+    PropertyHolder.framePaddingBottom = holder.frame.paddingBottom
+    PropertyHolder.framePaddingStart = holder.frame.paddingStart
+    PropertyHolder.framePaddingEnd = holder.frame.paddingEnd
+  }
+  
   override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): SeasonViewHolder {
     val v = LayoutInflater.from(parent!!.context).inflate(R.layout.season_list_item, parent, false)
     return SeasonViewHolder(v)
@@ -65,16 +63,18 @@ class SeasonAdapter(val context: Context,val seasonList: List<String>): Recycler
     return position.toLong()
   }
 
-  fun isDivider(name: String): Boolean {
-    when(name) {
-      "野菜" -> return true
-      "魚・貝" -> return true
-      "芋・キノコ" -> return true
-      "果物・きのみ" -> return true
-    }
-    return false
+  override fun getItemCount(): Int {
+    return seasonList.size
   }
 
+  override fun onBindViewHolder(holder: SeasonViewHolder?, position: Int) {
+    val pos = holder!!.itemId.toInt()
+    holder.name.text = seasonList[pos]
+    setPropertyHolder(holder)
+    if(isDivider(seasonList[pos])) dividerSetting(holder.frame, holder.name)
+    else columnSetting(holder.frame, holder.name)
+  }
+  
   class SeasonViewHolder(v: View): RecyclerView.ViewHolder(v) {
     val name: TextView = v.findViewById(R.id.season_list_item_name)
     val frame: LinearLayout = v.findViewById(R.id.season_list_item_frame)
@@ -82,14 +82,14 @@ class SeasonAdapter(val context: Context,val seasonList: List<String>): Recycler
 
   private object PropertyHolder {
     var nameSize: Float? = null
-    set(value) { field = field ?: value }
-    var framePaddginStart: Int? = null
-    set(value) { field = field ?: value}
-    var framePaddginEnd: Int? = null
-      set(value) { field = field ?: value}
-    var framePaddginTop: Int? = null
-      set(value) { field = field ?: value}
-    var framePaddginBottom: Int? = null
-      set(value) { field = field ?: value}
+      set(value) {field = field ?: value}
+    var framePaddingStart: Int? = null
+      set(value) {field = field ?: value}
+    var framePaddingEnd: Int? = null
+      set(value) {field = field ?: value}
+    var framePaddingTop: Int? = null
+      set(value) {field = field ?: value}
+    var framePaddingBottom: Int? = null
+      set(value) {field = field ?: value}
   }
 }
