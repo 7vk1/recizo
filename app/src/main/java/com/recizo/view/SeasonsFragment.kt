483c8bc4bc4d7d961ff23d9d691312c73b63e2a3
sugarnaoming <sakigithub@gmail.com>
2017-08-01 18:05:01 +0900

    Impl jump to now month

--- app/src/main/java/com/recizo/view/SeasonsFragment.kt
+++ app/src/main/java/com/recizo/view/SeasonsFragment.kt
@@ -4,12 +4,14 @@ import android.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.FragmentActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.recizo.R
 import com.recizo.presenter.TabAdapter
 import kotlinx.android.synthetic.main.fragment_seasons.*
+import java.util.*
 
 class SeasonsFragment : Fragment(){
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +26,10 @@ class SeasonsFragment : Fragment(){
   override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     val fa = activity as FragmentActivity
+    val month = Calendar.getInstance().get(Calendar.MONTH)
     season_tab.tabMode = TabLayout.MODE_SCROLLABLE
     seasons_viewpager.adapter = TabAdapter(fa.supportFragmentManager)
+    seasons_viewpager.setCurrentItem(month, true)
     season_tab.setupWithViewPager(seasons_viewpager)
   }
 
