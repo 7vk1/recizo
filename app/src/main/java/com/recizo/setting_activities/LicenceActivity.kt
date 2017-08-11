bfe1e771b91bf0d36b9c766d1823d38afe90b4e6
minakawa <minakawa.daiki.as@gmail.com>
2017-08-11 16:06:45 +0900

    add licenses

--- app/src/main/java/com/recizo/setting_activities/LicenceActivity.kt
+++ app/src/main/java/com/recizo/setting_activities/LicenceActivity.kt
@@ -3,10 +3,15 @@ package com.recizo.setting_activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.recizo.R
+import android.webkit.WebView
+
+
 
 class LicenceActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_licence)
+    val webView = findViewById(R.id.licence_view) as WebView
+    webView.loadUrl("file:///android_asset/licenses.html")
   }
 }
