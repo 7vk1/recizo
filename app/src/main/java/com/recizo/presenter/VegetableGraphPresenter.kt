8b8c841f8754d20640bfb66a77a94021373c823f
sugarnaoming <sakigithub@gmail.com>
2017-08-23 21:41:28 +0900

    Change graph description text

--- app/src/main/java/com/recizo/presenter/VegetableGraphPresenter.kt
+++ app/src/main/java/com/recizo/presenter/VegetableGraphPresenter.kt
@@ -16,7 +16,7 @@ class VegetableGraphPresenter(private val chart: LineChart) {
   var listener: LoadEventListener? = null
   val colors = HashMap<RecizoApi.Vegetables, Int>()
   init {
-    chart.description.text = "野菜の卸売価格　©︎ 2010 Agriculture & Livestock Industries Corporation"
+    chart.description.text = "野菜の卸売価格　独立行政法人農畜産業振興機構 「ベジ探」(原資料_農水省「青果物日別取扱高統計」)"
     chart.isHorizontalScrollBarEnabled = false
     chart.axisRight.setDrawLabels(false)
     chart.xAxis.axisMinimum = 0f
